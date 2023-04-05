f0f60bad9e663cc80d7685e229a803db0562f03f
Lumin0l <idelaiglesia.m@gmail.com>
2023-04-05 14:06:44 +0200

    operators list

---
+++ ex02_conditionals/conditionals_02_all_operators.py
@@ -0,0 +1,29 @@
+# Full list of operators.
+
+# == Equality
+# != Inequality
+# <> Inequality (deprecated)
+# >  Greater than
+# >= Greater than or equal to
+# < Less than
+# <= Less than or equal to
+
+# Examples:
+username = 'jon'
+
+if username == 'jon':
+    print('welcome')
+else:
+    print('not you')
+
+if username != 'jon':
+    print('good, not jon')
+else:
+    print("you're jon")
+
+number = 55
+
+if number < 100:
+    print('i am smaller than 100')
+else:
+    print('bigger than 100')
