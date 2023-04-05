a82efb9c34224d9c1d5a7763880d862a5684a748
Lumin0l <idelaiglesia.m@gmail.com>
2023-04-05 13:00:49 +0200

    conditionals overview

---
+++ ex02_conditionals/conditionals_00_overview.py
@@ -0,0 +1,18 @@
+# Let's check syntax
+
+age = 25
+
+if age < 25: # with spaces between symbol and ':' after.
+    print('sorry, you need to be at least 25')
+
+elif age > 100: # this allows for the introduction of another condition. 
+    print('sorry you need to be younger than a 100')
+
+else: # we can add an else condition after to do whatever when the condition is not met.
+    print("you're good to go")
+
+
+
+
+
+
