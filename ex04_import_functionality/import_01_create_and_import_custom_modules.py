64a50d981c83e3b4a18d1210b2101506fe5b7261
Lumin0l <idelaiglesia.m@gmail.com>
2023-04-11 13:24:01 +0200

    addition to import custom modules

--- ex04_import_functionality/import_01_create_and_import_custom_modules.py
+++ ex04_import_functionality/import_01_create_and_import_custom_modules.py
@@ -13,12 +13,18 @@ We create it like this:
     - create a python file (this very one).
     - add the functions we want.
     - save it.
-    - import + name_of_the_file.
+    - import + name_of_the_file (without the '.py'.
 
 To use functions inside the imported library we do it like this:
     Let's say the file is called "helper" and that we have a function called "greeting" that takes 2 arguments.
     - import helper
     - helper.greeting(one, two)
     And done.
+
+With that, it will import a file that is in the standard path, that is the same directory or the system if it's a third party lib, because they get auto aded to the systen.
+For files that are in different directories we need to use sys, because it's the standard path, and insert our new file into the sys:
+    Let's say that directory is called libs and that it's a directory outside of ours.
+    - import sys
+    - sys.path.insert(0, '../libs') -> '0' is the position where it will insert it, '0' meaning 'root sys'.
 
 '''
