3d95e2b969bc99c6400f06b7746b3292ee1d3ac9
Lumin0l <idelaiglesia.m@gmail.com>
2023-04-11 13:32:31 +0200

    import and alias

---
+++ ex04_import_functionality/import_03_import_and_alias.py
@@ -0,0 +1,12 @@
+'''
+Sometimes the names of the libs will be a bit clumsy to use in the program.
+For example, with the math library, you'll have to spell 'math.xxx()' all the time.
+
+To ease things up you can alias the name of the function and use that alias through the program:
+    Let's use the math library as an example.
+    - import math as m
+    - m.sqrt(25)
+
+And done, much easier.
+
+'''
