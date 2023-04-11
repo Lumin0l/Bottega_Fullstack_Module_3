4052c71b24aef60f144ffafd45d641d67e5cd114
Lumin0l <idelaiglesia.m@gmail.com>
2023-04-11 13:27:57 +0200

    import single function

---
+++ ex04_import_functionality/import_02_import_single_function.py
@@ -0,0 +1,10 @@
+'''
+Sometimes we will only want one function.
+
+For efficiency's sake, we can import only that one function like this:
+    Let's say we want to import 'sqrt', the math function for square root.
+    - from math import sqrt
+
+Note that you need to know both the name of the function and the name of the library that contains it both.
+'''
+
