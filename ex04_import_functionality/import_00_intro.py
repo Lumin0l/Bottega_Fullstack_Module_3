16ed5380e3430bd06cb2f966f31b802577214409
Lumin0l <idelaiglesia.m@gmail.com>
2023-04-11 12:48:51 +0200

    intro to import

---
+++ ex04_import_functionality/import_00_intro.py
@@ -0,0 +1,17 @@
+'''
+We have been working with the core python library (mostly).
+Within this core library there are some functions such as .format(), .range()...
+If we wanted extra functions, like specialized functions for using math (decimal, floor...) we need to import them to our system.
+Some of them, like the math library we have mentioned are included inside the core package, but others are third party libraries.
+
+For those we have the "Python Package Index" or PyPI, also called "the cheese shop".
+
+Inside of it we will have a large number of specialized libraries with functions:
+    - Data Science
+    - Flask
+    - Django
+    - ...
+
+To access them we need a connector, we are going to use "PIP" for that. It will allow us install those libraries into our system and let us use them,
+
+'''
