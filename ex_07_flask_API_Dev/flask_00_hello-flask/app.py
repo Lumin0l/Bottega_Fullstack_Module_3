c599df76f497b3a5c85f74568e6d3f71adc2db80
Lumin0l <idelaiglesia.m@gmail.com>
2023-04-21 11:38:37 +0200

    Intro to API development with flask

---
+++ ex_07_flask_API_Dev/flask_00_hello-flask/app.py
@@ -0,0 +1,16 @@
+# Exercise number 1 on the flask API course
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# Now we will do a route with a decorator.
+# A decorator is code that you can wrap around methods or classes.
+
+@app.route('/') # With a slash we say its our root.
+def hello():
+    return "Hey Flask"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
