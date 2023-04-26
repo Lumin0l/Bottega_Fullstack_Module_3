a8760dbd3570fa2decdbc3de7e13e1c73f1ea422
Lumin0l <idelaiglesia.m@gmail.com>
2023-04-26 17:34:57 +0200

    add one or many collections + additions to readme

--- ex08_mongoDB/mongo.js
+++ ex08_mongoDB/mongo.js
@@ -14,4 +14,60 @@ db.createUser({
 
 /* To visualize the users we use: db.getUsers() and to delete a user db.dropUser('name_of_user') */
 
-/* To create a collection we use: db.createCollection('name_of_collection') and to see it: show collections */ 
+/* To create a collection we use: db.createCollection('name_of_collection') and to see it: show collections */
+
+/* Inserting a document into mongodb database */
+db.books.insert({			// First database object and then the name of the collection
+	"name": "OOP Programming",
+	"PublishingDate": new Date(),
+	"authors": [
+		{"name": "John Snow"},
+		{"name": "NEd Stark" },
+		]
+	})
+
+/* How to insert many documents in one go */
+db.books.insertMany([
+	{
+		"name": "Ruby Intro",
+		"publishedDate": new Date(),
+		"authors: [
+			{"name": "Avdi Grimm" }
+			]
+	},
+	{
+                "name": "Ruby Intro",
+                "publishedDate": new Date(),
+                "authors: [
+                        {"name": "Avdi Grimm" }
+                        ]
+        },
+	{
+                "name": "Ruby Intro",
+                "publishedDate": new Date(),
+                "authors: [
+                        {"name": "Avdi Grimm" }
+                        ]
+        },
+	{
+                "name": "C Intro",
+                "publishedDate": new Date(),
+                "authors: [
+                        {"name": "Peter Grimm" }
+                        ]
+        },
+	{
+                "name": "Python Intro",
+                "publishedDate": new Date(),
+                "authors: [
+                        {"name": "John Grimm" }
+                        ]
+        }
+
+])
+
+
+
+
+
+
